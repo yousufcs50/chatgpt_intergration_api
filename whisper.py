ee8c6585e61b3648a73b8c15695a6125b01b00dc
Mohammad Yousuf <52100471+yousufcs50@users.noreply.github.com>
2023-05-08 20:35:28 +0500

    Update whisper.py

--- whisper.py
+++ whisper.py
@@ -1,5 +1,11 @@
 import openai
-openai.api_key = 
+import os
+from dotenv import load_dotenv
+
+# Load the API key from the environment or .env file
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = api_key
 file = open("recording.mp3", "rb")
 transcription = openai.Audio.transcribe("whisper-1", file)
 
